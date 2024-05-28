167755030f63ecbe35069e1fa642f5ea164d4a27
Takuma Watari <104181103+takumawatari@users.noreply.github.com>
2024-05-28 16:28:09 +0200

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -5,16 +5,6 @@ st.set_page_config(layout="wide")
 
 st.title("Interactive Sankey diagrams of iron and steel flows", anchor=None)
 
-tabs = st.tabs(["Overview", "Interact"])
-
-with tabs[0]:
-    st.markdown("**Author**: Takuma Watari (National Institute of Environmental Studies, Japan)")
-    st.markdown("**Aim**: This notebook presents interactive Sankey diagrams of iron and steel flows for the world's top 30 crude steel producing countries.")
-    st.markdown("**Data sources**: The primary data used in this analysis comes from the World Steel Association and the US Geological Survey.")
-    st.markdown("**Software**: The Sankey diagram is designed using floWeaver software: https://github.com/ricklupton/floweaver.")
-
-
-
 st.markdown("**Author**: Takuma Watari (National Institute of Environmental Studies, Japan)")
 st.markdown(
     "**Aim**: This web application presents interactive Sankey diagrams of iron and steel flows for the world's top 30 crude steel producing countries.")
@@ -29,10 +19,10 @@ st.markdown(
     "**-** View the graph: Once you've made your selection, the Sankey diagram for the selected country and year will instantly appear.")
 
 available_years = [2000, 2005, 2010, 2015, 2019]
-year = st.select_slider('Year', options=available_years)
+year = st.selectbox('Select Year:', available_years, help="Choose a year to explore the data.")
 file_path = f'data_{year}.xlsx'
 country_names_df = pd.read_excel(file_path, sheet_name='list')
-country = st.selectbox('Country', country_names_df['Country'])
+country = st.selectbox('Select Country:', country_names_df, help="Choose a country to view its Sankey diagram.")
 
 with open("sankey/" + f'{country}_{year}.svg', encoding="utf8") as file:
     svg_content = file.read()
