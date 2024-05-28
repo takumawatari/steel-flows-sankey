23e60c332723383aff4a0de085850554d084ab24
Takuma Watari <104181103+takumawatari@users.noreply.github.com>
2024-05-28 16:01:08 +0200

    update

--- main.py
+++
@@ -1,5 +0,0 @@
-# Interactive Sankey diagrams of iron and steel flows
-
-from sankey_interactive import run_sankey_interactive
-target_years = [2019] # To be updated
-run_sankey_interactive(target_years)
